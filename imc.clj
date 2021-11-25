a916bd9e35514082a015d783c33ba1df473b4cfd
Benedito Brito <benedito.brito@csdbr.com>
2021-11-25 14:37:56 -0300

    Criando arquivo imc.clj para teste

---
+++ imc.clj
@@ -0,0 +1,14 @@
+(ns compras.imc)
+
+
+(defn imc
+  [peso altura]
+  (if (< (/ peso (* altura altura)) 18.5)
+    :baixo-peso
+    (if (and (< (/ peso (* altura altura)) 24.9) (>= (/ peso (* altura altura)) 18.5))
+      :peso-ideal
+      :acima-peso)))
+
+
+(imc 1 2)
+
